a149ef2361842d7e7ca7a923775e7671ee9df92b
Glazny <zabelek50@gmail.com>
2025-04-15 16:03:12 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-                System.out.println("1. Piotr");
-                System.out.println("2. Anna");
-                System.out.println("3. Jan");
-                System.out.println("4. Leszek");
-            }
-        }
+  public static void main(String[] args) {
+    String[] imiona = {"Piotr", "Anna", "Jan", "Leszek"};
+
+    for (int i = 0; i < imiona.length; i++)
+    {
+    System.out.println(i + 1 + ". " + imiona[i]);
+    }
+  }
+}
