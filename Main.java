8ed9fa8931222cd7230c8b089c5b556110b9a5f8
Glazny <zabelek50@gmail.com>
2025-04-15 15:36:00 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        for (int i = 0; i < 6; i++) {
-            System.out.println("i = " + i);
+        System.out.println("Welcome to the Java Pipeline Demo!");
+        for (int i = 5; i >= 0; i--) {
+            System.out.println("Countdown: " + i);
         }
-        }
+        System.out.println("Launch complete!");
     }
+}
